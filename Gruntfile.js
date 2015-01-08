d3b47fd9eae49c021fab7a485456f691b2a4c0f4
KaiHofstetter <kai.hofstetter@gmx.de>
2015-01-08 20:28:41 +0100

    Initial version of link-stats.

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function (grunt) {
+    "use strict";
+    grunt.initConfig({
+            
+            pkg: grunt.file.readJSON('package.json'),
+            
+            jshint: {
+                all: ['Gruntfile.js', 'src/**/*.js', 'spec/**/*.js'],
+                options: {
+                    jshintrc: '.jshintrc'
+                }
+            },
+
+            jasmine: {
+                coverage: {
+                    src: ['src/*.js', '!src/link-stats.js'],
+                    options: {
+                        version: '2.1.0',
+                        specs: 'spec/**/*Spec.js',
+                        template: require('grunt-template-jasmine-istanbul'),
+                        templateOptions: {
+                            coverage: 'bin/coverage.json',
+                            report: [
+                                {
+                                    type: 'html',
+                                    options: {
+                                        dir: 'bin/html'
+                                    }
+                                },
+                                {
+                                    type: 'cobertura',
+                                    options: {
+                                        dir: 'bin/cobertura'
+                                    }
+                                },
+                                {
+                                    type: 'text-summary'
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+    );
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+    grunt.registerTask('test', ['jshint', 'jasmine:coverage']);
+    grunt.registerTask('default', ['test']);
+};

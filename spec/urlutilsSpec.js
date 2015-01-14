86ca1e5a94daf5a5f715c3db567d93e500646a9b
KaiHofstetter <kai.hofstetter@gmx.de>
2015-01-14 21:03:25 +0100

    UrlUtils tests rearranged.

--- spec/urlutilsSpec.js
+++ spec/urlutilsSpec.js
@@ -5,9 +5,7 @@ describe("UrlUtils", function () {
             var isSeflRef = UrlUtils.isSelfReference('http://wwww.yahoo.com');
             expect(isSeflRef).toBeFalsy();
         });
-    });
 
-    describe("isSelfReference", function () {
         it("should be able to detect '#test' as a self reference", function () {
             var isSeflRef = UrlUtils.isSelfReference('#test');
             expect(isSeflRef).toBeTruthy();
@@ -19,16 +17,12 @@ describe("UrlUtils", function () {
             var domain = UrlUtils.getDomain('https://www.blablub.com/sss');
             expect(domain).toEqual('www.blablub.com');
         });
-    });
 
-    describe("getDomain", function () {
         it("should be able to extract the domain 'www.adfas.org.uk' from 'http://www.adfas.org.uk/ ", function () {
             var domain = UrlUtils.getDomain('http://www.adfas.org.uk/');
             expect(domain).toEqual('www.adfas.org.uk');
         });
-    });
 
-    describe("getDomain", function () {
         it("should be able to extract the domain 'sub2.sub1.adfas.org.uk' from 'http://sub2.sub1.adfas.org.uk/", function () {
             var domain = UrlUtils.getDomain('http://sub2.sub1.adfas.org.uk/');
             expect(domain).toEqual('sub2.sub1.adfas.org.uk');

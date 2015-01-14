035fa57eda35479a48299bb4d5d0434a7fec9231
KaiHofstetter <kai.hofstetter@gmx.de>
2015-01-14 21:01:27 +0100

    UrlUtils can extract domains from URLs.

--- spec/urlutilsSpec.js
+++ spec/urlutilsSpec.js
@@ -13,4 +13,25 @@ describe("UrlUtils", function () {
             expect(isSeflRef).toBeTruthy();
         });
     });
+
+    describe("getDomain", function () {
+        it("should be able to extract the domain 'www.blablub.com' from 'https://www.blablub.com/sss", function () {
+            var domain = UrlUtils.getDomain('https://www.blablub.com/sss');
+            expect(domain).toEqual('www.blablub.com');
+        });
+    });
+
+    describe("getDomain", function () {
+        it("should be able to extract the domain 'www.adfas.org.uk' from 'http://www.adfas.org.uk/ ", function () {
+            var domain = UrlUtils.getDomain('http://www.adfas.org.uk/');
+            expect(domain).toEqual('www.adfas.org.uk');
+        });
+    });
+
+    describe("getDomain", function () {
+        it("should be able to extract the domain 'sub2.sub1.adfas.org.uk' from 'http://sub2.sub1.adfas.org.uk/", function () {
+            var domain = UrlUtils.getDomain('http://sub2.sub1.adfas.org.uk/');
+            expect(domain).toEqual('sub2.sub1.adfas.org.uk');
+        });
+    });
 });
--- src/urlutils.js
+++ src/urlutils.js
@@ -1,5 +1,13 @@
 var UrlUtils = {
     isSelfReference: function (path) {
         return path.match(/^#/);
+    },
+    
+    getDomain: function (url) {
+        var urlRegex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+        
+        var urlGroups = urlRegex.exec(url);
+
+        return urlGroups[2]+'.'+urlGroups[3];
     }
 };
